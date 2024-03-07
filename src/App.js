9d9844d4485cec94bafe9c79a138a2aff8e79a7e
CYBERFENRIR <kumar.aayush04@gmail.com>
2024-03-07 11:04:26 +0530

    netlify config

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
       <div>
         <Routes>
           <Route exact path="/" element={<Products />} />
-          <Route path = "/admin" element= {<AdminPage />}/>
-          <Route path = "/addProduct" element= {<AddProduct />}/>
+          <Route exact path = "/admin" element= {<AdminPage />}/>
+          <Route exact path = "/addProduct" element= {<AddProduct />}/>
         </Routes>
       </div>
     </Router>
